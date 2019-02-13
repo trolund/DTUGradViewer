4c561c4ee63bf407035a854e4c76b40d1180d0a7
trolund@gmail.com <vxh77kdp>
2019-02-13 19:26:33 +0100

    Initial Commit

---
+++ DTUGradViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DTUGradViewer
+//
+//  Created by Troels on 13/02/2019.
+//  Copyright © 2019 Troels. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
